b0ddc826a9938b4453d687fea73b3d64bf1b72e4
engwesterberg <erikwesterberg92@gmail.com>
2020-08-24 23:45:16 +0200

    Added google sign in button

--- Carina/components/LoginScreen.js
+++ Carina/components/LoginScreen.js
@@ -1,5 +1,5 @@
 import {COLORS} from '../colors.js';
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import AppButton from './AppButton.js';
 import Header from './Header';
 import Hr from 'react-native-hr-component';
@@ -10,12 +10,16 @@ import {
   View,
   Text,
   StyleSheet,
-  TextInput,
   Dimensions,
   ImageBackground,
-  Modal,
   KeyboardAvoidingView,
 } from 'react-native';
+import {
+  GoogleSignin,
+  GoogleSigninButton,
+  statusCodes,
+} from '@react-native-community/google-signin';
+
 const windowWidth = Dimensions.get('window').width;
 const windowHeight = Dimensions.get('window').height;
 
@@ -26,6 +30,38 @@ const LoginScreen = (props) => {
   const [secret, setSecret] = useState('');
   const [repeatSecret, setRepeatSecret] = useState('');
   const [signupSuccess, setSignupSuccess] = useState(false);
+
+  useEffect(() => {
+    GoogleSignin.configure({
+      scopes: ['https://www.googleapis.com/auth/drive.readonly'], // what API you want to access on behalf of the user, default is email and profile
+      webClientId:
+        '564449950849-1g9n892jm9q4mu9tigs18f38jbgdqgso.apps.googleusercontent.com', // client ID of type WEB for your server (needed to verify user ID and offline access)
+      offlineAccess: true, // if you want to access Google API on behalf of the user FROM YOUR SERVER
+      //hostedDomain: '', // specifies a hosted domain restriction
+      //loginHint: '', // [iOS] The user's ID, or email address, to be prefilled in the authentication UI if possible. [See docs here](https://developers.google.com/identity/sign-in/ios/api/interface_g_i_d_sign_in.html#a0a68c7504c31ab0b728432565f6e33fd)
+      forceCodeForRefreshToken: true, // [Android] related to `serverAuthCode`, read the docs link below *.
+      //accountName: '', // [Android] specifies an account name on the device that should be used
+      //iosClientId: '<FROM DEVELOPER CONSOLE>', // [iOS] optional, if you want to specify the client ID of type iOS (otherwise, it is taken from GoogleService-Info.plist)
+    });
+  }, []);
+
+  const googleSignIn = async () => {
+    try {
+      await GoogleSignin.hasPlayServices();
+      const userInfo = await GoogleSignin.signIn();
+      this.setState({userInfo});
+    } catch (error) {
+      if (error.code === statusCodes.SIGN_IN_CANCELLED) {
+        // user cancelled the login flow
+      } else if (error.code === statusCodes.IN_PROGRESS) {
+        // operation (e.g. sign in) is in progress already
+      } else if (error.code === statusCodes.PLAY_SERVICES_NOT_AVAILABLE) {
+        // play services not available or outdated
+      } else {
+        // some other error happened
+      }
+    }
+  };
 
   return (
     <ImageBackground
@@ -84,6 +120,11 @@ const LoginScreen = (props) => {
                   });
               }}
             />
+             <GoogleSigninButton
+    style={{ width: 192, height: 48 }}
+    size={GoogleSigninButton.Size.Wide}
+    color={GoogleSigninButton.Color.Dark}
+               onPress={() => { console.warn("clickos") }}/>
           </View>
         ) : (
           <View style={styles.loginContainer}>
